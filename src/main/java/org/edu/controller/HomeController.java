598fb5eb2d6d0cef8950a9638a7f2ddb0ae1d7e5
hu-24 <hu-24@hu-24-PC>
2020-06-09 14:56:25 +0900

    포트폴리오

--- src/main/java/org/edu/controller/HomeController.java
+++ src/main/java/org/edu/controller/HomeController.java
@@ -1,89 +1,193 @@
 package org.edu.controller;
 
+ 
+
 import java.text.DateFormat;
+
 import java.util.Date;
+
 import java.util.Locale;
 
+ 
+
 import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestMethod;
 
+ 
+
 /**
+
  * Handles requests for the application home page.
+
  */
+
 @Controller
+
 public class HomeController {
+
 	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 	
+
 	/**
-	 * we are 테스트용 파일 입니다.
+
+	 * we are 페이지 파일 입니다.
+
 	 */
-	@RequestMapping(value = "/weare", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/sample/weare", method = RequestMethod.GET)
+
 	public String weare(Locale locale, Model model) {
+
 		
-		return "weare";
+
+		return "sample/weare";
+
 	}
-	
+
 	/**
-	 * work 테스트용 파일 입니다.
+
+	 * WORK 페이지 파일 입니다.
+
 	 */
-	@RequestMapping(value = "/work", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/sample/work", method = RequestMethod.GET)
+
 	public String work(Locale locale, Model model) {
+
 		
-		return "work";
+
+		return "sample/work";
+
 	}
-	
+
 	/**
-	 * blog 테스트용 파일 입니다.
+
+	 * BLOG 페이지 파일 입니다.
+
 	 */
-	@RequestMapping(value = "/blog", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/sample/blog", method = RequestMethod.GET)
+
 	public String blog(Locale locale, Model model) {
+
 		
-		return "blog";
+
+		return "sample/blog";
+
 	}
-	
+
 	/**
-	 * work 테스트용 파일 입니다.
+
+	 * CONTACT US 페이지 파일 입니다.
+
 	 */
-	@RequestMapping(value = "/slide", method = RequestMethod.GET)
-	public String slide(Locale locale, Model model) {
-		
-		return "slide";
-	}
-	
-	/**
-	 * contact us 테스트용 파일 입니다.
-	 */
-	@RequestMapping(value = "/contactus", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/sample/contactus", method = RequestMethod.GET)
+
 	public String contactus(Locale locale, Model model) {
+
 		
-		return "contactus";
+
+		return "sample/contactus";
+
 	}
-	
-	
-	
-	
-	
-	
+
 	/**
-	 * Simply selects the home view to render by returning its name.
+
+	 * HTML 페이지 파일 입니다.
+
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+
+	@RequestMapping(value = "/sample/htmltest", method = RequestMethod.GET)
+
+	public String htmltest(Locale locale, Model model) {
+
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+
+		return "sample/htmltest";
+
 	}
+
 	
+
+	/**
+
+	   slide 페이지 파일 입니다.
+
+	 */
+
+	@RequestMapping(value = "/sample/slide", method = RequestMethod.GET)
+
+	public String slide(Locale locale, Model model) {
+
+		
+
+		return "sample/slide";
+
+	}
+
+	/**
+
+	   sample 파일 홈 입니다.
+
+	 */
+
+	@RequestMapping(value = "/sample/home", method = RequestMethod.GET)
+
+	public String sample(Locale locale, Model model) {
+
+		
+
+		return "sample/home";
+
+	}
+
+	
+
+	/**
+
+	 * Simply selects the home view to render by returning its name.
+
+	 */
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+
+	public String home(Locale locale, Model model) {
+
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		
+
+		Date date = new Date();
+
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		
+
+		String formattedDate = dateFormat.format(date);
+
+		
+
+		model.addAttribute("serverTime", formattedDate );
+
+		
+
+		return "home";
+
+	}
+
+	
+
 }
