f3177f551ce05d65d37cdd01f18e69c108b4879a
Admin <Admin@lee-PC>
2020-07-21 10:34:07 +0900

    heroku 배포용

--- src/main/java/org/edu/vo/PageVO.java
+++ src/main/java/org/edu/vo/PageVO.java
@@ -32,21 +32,21 @@ public void setSearchKeyword(String searchKeyword) {
 
 private void calcPage() {
       //page변수는 현재 jsp에서 클릭한 페이지 번호
-      int tempEnd = (int)(Math.ceil(page/10.0)*10);
+      int tempEnd = (int)(Math.ceil(page/(double)this.perPageNum)*this.perPageNum);
       //ceil 함수는 천장 함수로 1.1 = 2, 2.1 = 3 으로 출력된다.
       //반대되는 바닥함수로 floor(), 반올림 함수로 round()가 있다.
       //jsp에서 클릭한 페이지 번호를 기준으로 끝 페이지를 계산한다.
-      this.startPage = tempEnd - 9;
+      this.startPage = (tempEnd - this.perPageNum)+1;
       //시작 페이지 계산 클릭한 page번호 10일때 까지 시작페이지는 1
-      if(tempEnd * 10 > this.totalCount) {
+      if(tempEnd * this.perPageNum > this.totalCount) {
          //클릭한 page번호로 계산된 게시물수가 실제 게시물 개수 totalCount 클때
-         this.endPage = (int)Math.ceil(this.totalCount/10.0);
+         this.endPage = (int)Math.ceil(this.totalCount/(double)this.perPageNum);
       }else {
          //클릭한 page번호로 계산된 게시물수가 실제 게시물 개수 totalCount 작을때
          this.endPage = tempEnd;
       }
       this.prev = this.startPage != 1;// 시작 페이지가 1보다 크면 무조건 이전 페이지가 있다. true
-      this.next = this.endPage * 10 < this.totalCount;
+      this.next = this.endPage * this.perPageNum < this.totalCount;
       //클릭한 page번호로 계산된 게시물수가 실제 게시물 개수보다 작다면 다음페이지가 있음. true
    }
    
